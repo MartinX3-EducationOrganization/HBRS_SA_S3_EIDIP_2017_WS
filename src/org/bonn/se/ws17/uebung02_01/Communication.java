f65c13580a785040e53cb867e34278c9c3612eb5
Terenceskill <reinholdjourdan@hotmail.com>
2018-02-08 16:47:32 +0100

    close #3

---
+++ src/org/bonn/se/ws17/uebung02_01/Communication.java
@@ -0,0 +1,7 @@
+package org.bonn.se.ws17.uebung02_01;
+
+public class Communication {
+    public static String hello(String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/org/bonn/se/ws17/uebung02_01/HelloWithCommunication.java
@@ -0,0 +1,7 @@
+package org.bonn.se.ws17.uebung02_01;
+
+public class HelloWithCommunication {
+    public static void main(String[] args) {
+        System.out.println(Communication.hello("World"));
+    }
+}
