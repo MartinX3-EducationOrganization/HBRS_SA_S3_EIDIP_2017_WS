0653279131379c7b5eb0f342c885d4a5ec20422f
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2018-02-10 13:49:41 +0100

    Task 13

---
+++ src/org/bonn/se/ws17/uebung13_06/Zeitung.java
@@ -0,0 +1,13 @@
+package org.bonn.se.ws17.uebung13_06;
+
+import java.io.*;
+class Schriftstueck {
+  void lese() throws IOException {
+  }
+}
+public class Zeitung extends Schriftstueck {
+  public static void main(String[] args) throws IOException {
+    new Zeitung().lese();
+  }
+  // Deklaration von lese()
+}
