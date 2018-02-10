4c42d6d0d071d5ffe8be2e558ec43d1dc110982e
Terenceskill <reinholdjourdan@hotmail.com>
2018-02-10 15:00:48 +0100

    close #27

--- src/org/bonn/se/ws17/uebung07_03/DatumMitFehler.java
+++ src/org/bonn/se/ws17/uebung07_03/DatumMitFehler.java
@@ -1,20 +1,20 @@
 package org.bonn.se.ws17.uebung07_03;
 
-public class DatumMitTeuremFehler {
-    // Hinweis: ja, so wird teilweise in der Industrie programmiert...
-    public static int berechneJahr(int tage) {
-        int jahr = 1980; // 01.01.1980 ist Tag 1
-        while (tage > 365) {
-            if (Zeit.istSchaltjahr(jahr)) {
-                if (tage > 366) {
-                    tage = tage - 366;
-                    jahr = jahr + 1;
-                }
-            } else {
-                tage = tage - 365;
-                jahr = jahr + 1;
-            }
-        }
-        return jahr;
-    }
-}
+//public class DatumMitTeuremFehler {
+//    // Hinweis: ja, so wird teilweise in der Industrie programmiert...
+//    public static int berechneJahr(int tage) {
+//        int jahr = 1980; // 01.01.1980 ist Tag 1
+//        while (tage > 365) {
+//            if (Zeit.istSchaltjahr(jahr)) {
+//                if (tage > 366) {
+//                    tage = tage - 366;
+//                    jahr = jahr + 1;
+//                }
+//            } else {
+//                tage = tage - 365;
+//                jahr = jahr + 1;
+//            }
+//        }
+//        return jahr;
+//    }
+//}
