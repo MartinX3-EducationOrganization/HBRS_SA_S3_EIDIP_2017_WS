31c50cf104132a977cff8a02515b57c2b3035cd4
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2018-02-10 13:19:34 +0100

    Task 12

--- src/org/bonn/se/ws17/uebung11_03/Arbeiten.java
+++ src/org/bonn/se/ws17/uebung11_03/Arbeiten.java
@@ -1,42 +1,44 @@
 package org.bonn.se.ws17.uebung11_03;
 
 public class Job {
-  public static void zweck() {
-    System.out.println("bringt Geld");
-  }
+    public static void zweck() {
+        System.out.println("bringt Geld");
+    }
 
-  public void start() {
-    System.out.println("los!");
-  }
+    public void start() {
+        System.out.println("los!");
+    }
 
-  public void stop() {
-    System.out.println("fertig!");
-  }
+    public void stop() {
+        System.out.println("fertig!");
+    }
 
-  public void erledigen() {
-    start();
-    stop();
-  }
+    public void erledigen() {
+        start();
+        stop();
+    }
 }
 
 public class Programmieraufgabe extends Job {
-  public static void zweck() {
-    System.out.println("rettet die Welt");
-  }
+    public static void zweck() {
+        System.out.println("rettet die Welt");
+    }
 
-  public void start() {
-    System.out.println("Kaffee holen!");
-  }
+    @Override
+    public void start() {
+        System.out.println("Kaffee holen!");
+    }
 
-  public void stop() {
-    System.out.println("Loesung in den Praktomat laden!");
-  }
+    @Override
+    public void stop() {
+        System.out.println("Loesung in den Praktomat laden!");
+    }
 }
 
 public class Arbeiten {
-  public static void main(String[] args) {
-    Job job = new Programmieraufgabe();
-    job.erledigen();
-    job.zweck();
-  }
+    public static void main(String[] args) {
+        Job job = new Programmieraufgabe();
+        job.erledigen();
+        job.zweck();
+    }
 }
---
+++ src/org/bonn/se/ws17/uebung12_04/Main.java
@@ -0,0 +1,7 @@
+package org.bonn.se.ws17.uebung12_04;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
