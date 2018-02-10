74549efa9b446dbaeee3a14d8f0e094ec90aaaaa
Terenceskill <reinholdjourdan@hotmail.com>
2018-02-10 22:26:09 +0100

    close #7

--- src/org/bonn/se/ws17/uebung02_05/Zeichenketten.java
+++ src/org/bonn/se/ws17/uebung02_05/Zeichenketten.java
@@ -5,23 +5,20 @@ public class Zeichenketten {
     public static String halloMitZeit(int stunde, String name) {
 
         if (stunde < 12) {
-            return String.format("Guten Morgen, %s !", name);
+            return String.format("Guten Morgen, %s!", name);
         } else if (stunde >= 18) {
-            return String.format("Guten Abend, %s !", name);
+            return String.format("Guten Abend, %s!", name);
         } else {
-            return String.format("Guten Tag, %s !", name);
+            return String.format("Guten Tag, %s!", name);
         }
     }
 
-    public static String zitronenEis(int n) throws Exception {
-        if (n < 0) {
-            throw new Exception("Keine negativen Zahlen!");
-        }
+    public static String zitronenEis(int n) {
         String s = "", z = "";
         for (int i = 0; i < n; i++) {
             s += "super";
             z += "Z";
         }
-        return String.format("%sleckers %sZitroneneis", s, z);
+        return String.format("%sleckeres %sZitroneneis", s, z);
     }
 }
