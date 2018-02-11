4209b4adada3e7ecc8dc0421b1d50b23a1ac7b5a
Terenceskill <reinholdjourdan@hotmail.com>
2018-02-11 16:23:51 +0100

    close #30 Schleife eingebaut.

--- src/org/bonn/se/ws17/uebung06_04/Zahlen.java
+++ src/org/bonn/se/ws17/uebung06_04/Zahlen.java
@@ -3,13 +3,16 @@ package org.bonn.se.ws17.uebung06_04;
 public class Zahlen {
     public static int ggT(int a, int b) {
         if (a < 0) {
-            return Zahlen.ggT(-a, b);
-        } else if (b < 0) {
-            return Zahlen.ggT(a, -b);
-        } else if (b == 0) {
-            return a;
-        } else {
-            return Zahlen.ggT(b, a % b);
+            a = a * (-1);
         }
+        if (b < 0) {
+            b = b * (-1);
+        }
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
