18aaa0ae0c5fa01cd30128fac75a593eed6507cf
Terenceskill <reinholdjourdan@hotmail.com>
2018-02-12 16:24:47 +0100

    close #44

--- src/org/bonn/se/ws17/uebung08_05/Main.java
+++
@@ -1,7 +0,0 @@
-package org.bonn.se.ws17.uebung08_05;
-
-public class Main {
-    public static void main(String[] args) {
-        // write your code here
-    }
-}
---
+++ src/org/bonn/se/ws17/uebung08_05/Zahlen.java
@@ -0,0 +1,42 @@
+package org.bonn.se.ws17.uebung08_05;
+
+public class Zahlen {
+    // b = potenz (a^b)
+    public static int potenzLoop(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+        while (b > 1) {
+            if (b % 2 == 0) {
+
+                a *= a;
+                b /= 2;
+
+            } else if (b % 2 != 0) {
+                int c = a;
+                a *= a;
+                b /= 2;
+                a = (a * b) * c;
+            }
+        }
+        return a;
+    }
+
+    public static int potenzRek(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+        return Zahlen.potenzR(a, b);
+    }
+
+    private static int potenzR(int a, int b) {
+        if (b == 1) {
+            return a;
+        }
+        if (b % 2 == 0) {
+            return Zahlen.potenzR(a * a, b / 2);
+        } else {
+            return Zahlen.potenzR(a * a, b / 2) * a;
+        }
+    }
+}
